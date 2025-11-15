51a138a226f6a9eef1656f60fb0733ccd43a288d
srinivas <bathula458@gmail.com>
2025-11-15 09:53:19 +0530

    Add new files

---
+++ a.js
@@ -0,0 +1,25 @@
+// function savetodb(data,success,failure){
+//     let internetspeed=Math.floor(Math.random()*10)+1;
+//     if(internetspeed>4){
+//         success();
+//     }
+//     else{
+//         failure();
+//     }
+//     }
+//     savetodb(
+
+//         'my college',
+//         ()=>{
+//             console.log('ýour data was saved');
+//         },
+//         ()=>{
+//             console.log('weak connection');
+//         }
+//     );
+
+// async function greet() {
+//     return 'hello pukka';
+    
+// } 
+
